8cb5ed947433964e92bbed29e3a6950df2a2e879
barryflux <aluridja@gmail.com>
2026-06-20 17:39:38 +0200

    Remove duplicated logo service interface

--- FntrAudit/Services/Logos/ILogoService.cs
+++
@@ -1,9 +0,0 @@
-using FntrAudit.Models;
-
-namespace FntrAudit.Services.Logos
-{
-    public interface ILogoService
-    {
-        Task<List<Logo>> GetAllLogoAsync(CancellationToken cancellationToken = default);
-    }
-}
